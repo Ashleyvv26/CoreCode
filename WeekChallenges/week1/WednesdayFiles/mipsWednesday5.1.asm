bbd060f57766cdd2366b191cd5eb4a2212b33b15
Ashleyvv26 <97487154+Ashleyvv26@users.noreply.github.com>
2022-01-13 00:39:21 -0600

    Changed directory

---
+++ WeekChallenges/week1/WednesdayFiles/mipsWednesday5.1.asm
@@ -0,0 +1,30 @@
+.data
+	number1: .asciiz "\nIngrese el primer numero: "
+	number2: .asciiz "\nIngrese el segundo numero: "
+	result_message: .asciiz "\nEl resultado de la sumatoria es: "
+.text
+	main:
+		li $v0, 4
+		la $a0, number1
+		syscall
+
+		li $v0, 5
+		syscall
+
+		move $t0, $v0
+
+		li $v0, 4
+		la $a0, number2
+		syscall
+
+		li $v0, 5
+		syscall
+
+		move $t1, $v0
+		
+		add $t3, $t0, $t1
+
+		li $v0, 1
+		move $a0, $t3
+		syscall
+		
