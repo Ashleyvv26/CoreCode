6910cb007c35aa414dffe5a561222532ad222fde
Ashley Valdez <97487154+Ashleyvv26@users.noreply.github.com>
2022-04-08 14:42:49 -0600

    Create main.ts

---
+++ WeekChallenges/week6/tuesday/main.ts
@@ -0,0 +1,56 @@
+import { Movie } from "./Movie";
+import {ActionMovie} from "./ActionMovie" ;
+import { HorrorMovie } from "./HorrorMovie";
+
+export class Main {
+    actionMovies:String[] = [] ;  
+    horrorMovies:String[] = [] ;  
+
+
+
+
+
+
+
+    start() {
+        
+        
+
+
+
+
+
+
+
+
+
+    }
+
+    addActionMovie() : string{
+        return ''
+    }
+    addHorrorMovie() : string {
+        return ''
+    }
+
+    showActionMovies() : void {
+
+    }
+    showHorrorMovies() : void {
+        
+    }
+    incrementExplosion() : void {
+
+    }
+    incrementJumpscares() : void {
+
+    }
+
+
+
+
+
+
+
+
+}
