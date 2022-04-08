fb2dfb7c2ffdc5dc5479d56385d58889dc21ed56
Ashley Valdez <97487154+Ashleyvv26@users.noreply.github.com>
2022-04-08 14:46:45 -0600

    Create board.ts

---
+++ WeekChallenges/week7/monday/board.ts
@@ -0,0 +1,22 @@
+type valores = '👻'|'🎃' ;
+export class Board {
+    tablero :valores[][]=[][] ;
+
+    xGhost:string='👻'
+    xPumpkin:string='🎃'
+
+    // private  rows:number=3
+    // private  columns:number=3
+
+    constructor( private  rows:number=3,private  columns:number=3) {}
+
+    initialiceMatrix() {
+
+    }
+    clearMatrix(){
+
+    }
+
+    
+
+}
